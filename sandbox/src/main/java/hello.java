42c94ad96ba4833b87fb09e65835c5ebcf291f4c
MariaBorodina <msborodina@gmail.com>
2024-11-16 22:52:53 +0600

    Эксперименты с функциями (теперь точно)

--- sandbox/src/main/java/hello.java
+++ sandbox/src/main/java/hello.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println((2+2)*2);
+
+        System.out.println("Hello " + "world");
+        System.out.println("2 + 2 = " + (2+2) );
+    }
 }
